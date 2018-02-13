7ef261dc4de3e88666e8710722f2d851c843e0bc
Charles Rosaaen <charlesrosaaen@gmail.com>
2018-02-12 16:30:19 -0800

    Pagejumper fully disables when no pages

--- GameRaven/src/main/java/com/ioabsoftware/gameraven/AllInOneV2.java
+++ GameRaven/src/main/java/com/ioabsoftware/gameraven/AllInOneV2.java
@@ -2419,7 +2419,12 @@ public class AllInOneV2 extends AppCompatActivity implements SwipeRefreshLayout.
         getSupportActionBar().setTitle(titleIn);
 
         if (currPage == -1) {
-            pageLabel.setText("~ 1 / 1 ~");
+            pageLabel.setText("1 / 1");
+            firstPage.setEnabled(false);
+            prevPage.setEnabled(false);
+            nextPage.setEnabled(false);
+            lastPage.setEnabled(false);
+            pageLabel.setEnabled(false);
         } else {
             pageJumperDesc = desc;
 
